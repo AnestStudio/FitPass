b2309a8db55c5ae1adf294cef51e07710e55f85a
AnhDT <anest.academy@gmail.com>
2023-09-21 10:09:46 +0700

    fix anotation

--- fitpass-web/src/main/java/com/ks/fitpass/web/FitPassWebApplication.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/FitPassWebApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("com")
 @EntityScan("com")
+@ComponentScan("com")
+@SpringBootApplication
 public class FitPassWebApplication {
 
     public static void main(String[] args) {
